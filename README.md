27f6186dc82a401d78eaac71b56315ac1a552b2c
Prameya Mohanty <prameyamohanty14@gmail.com>
2024-08-25 01:54:08 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# FemCare - Insightful Wellness 🌸
+
+## Project by:
+- **Upasana Pradhan**
+- **Prameya Mohanty**
+
+**Class:** XI  
+**School:** Delhi Public School, Rourkela
+
+---
+
+## 📜 Problem Statement:
+The alarming rise in gynecological diseases such as cervical cancer, breast cancer, ovarian cancer, and polycystic ovarian syndrome (PCOS) poses a significant public health challenge globally and in India. 
+
+- **Cervical Cancer:** 
+  - 604,000 new cases and 342,000 deaths worldwide annually.
+  - India contributes to 30% of the global burden, with over 123,000 new cases and 77,000 deaths each year.
+
+- **Breast Cancer:** 
+  - Leading cause of cancer incidence globally with 2.3 million new cases in 2020.
+  - In India, nearly 60% of cases are diagnosed at stages III and IV, leading to higher mortality rates.
+
+- **Ovarian Cancer:** 
+  - More than 313,000 new cases worldwide in 2020.
+  - In India, it constitutes 3.44% of all cancer cases with many diagnosed at advanced stages.
+
+- **Polycystic Ovarian Syndrome (PCOS):** 
+  - Affects 8-13% of women globally, leading to infertility and long-term health issues.
+  - In India, the prevalence ranges from 8-22%, with diagnosis and treatment delays due to socio-cultural barriers and lack of awareness.
+
+The urgency to address these diseases through early diagnosis, efficient treatment, and increased awareness is paramount to improving women's health outcomes globally.
+
+---
+
+## 💡 Solution:
+**FemCare** aims to revolutionize gynecological healthcare by leveraging advanced prediction and classification algorithms to enable early diagnosis and effective management of cervical cancer, breast cancer, ovarian cancer, and polycystic ovarian syndrome (PCOS). 
+
+### Key Features:
+- **Advanced Data Analysis & Machine Learning Models:**
+  - Accurate risk assessment tools for healthcare professionals.
+  - Timely interventions and improved patient outcomes.
+
+- **Comprehensive Patient Data Integration:**
+  - Enhances women's health and well-being globally.
+
+- **Educational Resources & Community Support:**
+  - Proactive healthcare solutions.
+  - Supportive environment for shared experiences and expert guidance.
+
+**FemCare** is committed to promoting early detection, informed decisions, and healthy outcomes for women worldwide. 
+
+---
+
+## 🌍 SDG Goal:
+### SDG #3: Good Health and Well-being
+SDG 3 aims to ensure healthy lives and promote well-being for all at all ages. Specifically, the project addresses several targets within this goal:
+
+- **Target 3.4:** Reduce by one third premature mortality from non-communicable diseases through prevention, treatment, and the promotion of mental health and well-being.
+- **Target 3.7:** Ensure universal access to sexual and reproductive health-care services, including for family planning, information, and education, and the integration of reproductive health into national strategies and programs.
+- **Target 3.8:** Achieve universal health coverage, including financial risk protection, access to quality essential health-care services, and access to safe, effective, quality, and affordable essential medicines and vaccines for all.
+
+By focusing on early diagnosis and effective management of gynecological diseases like cervical cancer, breast cancer, ovarian cancer, and PCOS, this project directly contributes to reducing mortality, improving health outcomes, and enhancing the quality of life for women.
+
+---
---
+++ app.py
@@ -0,0 +1,250 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
+from keras.models import model_from_json
+import random
+import os
+from werkzeug.utils import secure_filename
+import shutil
+from flask_pymongo import PyMongo
+# import json
+import requests
+import tensorflow as tf
+
+app = Flask(__name__, static_folder="Static", template_folder="Templates")
+
+# Journal Setup from MongoDB
+
+app.config["MONGO_URI"] = "mongodb+srv://prameyamohanty10:PrAmEyAmOhAnTy%40100808@mydatabase.gyir6sh.mongodb.net/FemCare?retryWrites=true&w=majority&appName=MyDatabase/FemCare"
+mongo = PyMongo(app)
+
+# ========================================================================================
+
+# Journal Setup from JSON
+
+# with open("FemCare.articles.json", "r") as journal:
+#     data = json.load(journal)
+
+# ========================================================================================
+
+# Cervical Cancer Setup
+
+model = joblib.load("models\cervical_cancer.joblib")
+features = [["Age", "Number of sexual partners", "First sexual intercourse", "Num of pregnancies", "Smokes (years)", "Smokes (packs/year)", "Hormonal Contraceptives (years)", "IUD (years)", "STDs: Number of diagnosis"], ["condylomatosis", "cervical condylomatosis", "vaginal condylomatosis", "vulvo-perineal condylomatosis", "syphilis", "pelvic inflammatory disease", "genital herpes", "molluscum contagiosum", "AIDS", "HIV", "Hepatitis B", "HPV"], ["Cancer", "CIN", "hpv"]]
+
+mainsfeatures = ["Age", "Number of sexual partners", "First sexual intercourse", "Num of pregnancies", "Smokes (years)", "Smokes (packs/year)", "Hormonal Contraceptives (years)", "IUD (years)", "STDs", "STDs (number)", "condylomatosis", "cervical condylomatosis", "vaginal condylomatosis", "vulvo-perineal condylomatosis", "syphilis", "pelvic inflammatory disease", "genital herpes", "molluscum contagiosum", "AIDS", "HIV", "Hepatitis B", "HPV", "STDs: Number of diagnosis", "Cancer", "CIN", "HPV"]
+
+# ========================================================================================
+
+# Breast Cancer Setup
+
+json_file = open(r"models\breast_cancer.json", "r")
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights(r"models\breast_cancer.h5")
+
+loaded_model.compile(
+    loss="binary_crossentropy", optimizer="rmsprop", metrics=["accuracy"]
+)
+
+def save_and_get_pred_img(image):
+    defrance = str(random.randint(1, 100000))
+    
+    file = r"C:\Users\prame\OneDrive - 45ktw4\Documents\Intel India AI Impact Festival 2024\FemCare"
+    file_path = os.path.join(file, defrance)
+    os.makedirs(file_path)
+    filename = secure_filename(image.filename)
+    next_file_path = os.path.join(file_path, defrance)
+    os.makedirs(next_file_path)
+    UPLOAD_FOLDER = next_file_path
+    app.config["IMAGE_UPLOADS"] = UPLOAD_FOLDER
+    image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+    return file_path
+
+class Api_service:
+    def __init__(self, img_file_path):
+        self.img_file_path = img_file_path
+
+    def prediction_function(self):
+        data_generation = ImageDataGenerator(rescale=1.0 / 255)
+        predict_generation = data_generation.flow_from_directory(
+            self.img_file_path,
+            target_size=(25, 25),
+            batch_size=10,
+            class_mode="categorical",
+        )
+
+        prediction = loaded_model.predict_generator(predict_generation)
+        has_no_cancer = (
+            "The percentage of no cancer : "
+            + str(round(prediction[0][1] * 100, 2))
+            + "%"
+        )
+        has_cancer = (
+            "The percentage of being affected by Breast Cancer is " + str(round(prediction[0][0] * 100, 2)) + "%."
+        )
+        shutil.rmtree(self.img_file_path)
+        return has_cancer, has_no_cancer
+    
+# ========================================================================================
+
+# PCOS Setup
+
+pcos_features = ['Age (yrs)', 'Weight (Kg)', 'Height(Cm)', 'BMI', 'Blood Group', 'Pulse rate(bpm)', 'RR (breaths/min)', 'Hb(g/dl)', 'Cycle(R/I)', 'Cycle length(days)', 'Marraige Status (Yrs)', 'Pregnant(Y/N)', 'No. of aborptions', 'I   beta-HCG(mIU/mL)', 'II    beta-HCG(mIU/mL)', 'FSH(mIU/mL)', 'LH(mIU/mL)', 'FSH/LH', 'Hip(inch)', 'Waist(inch)', 'Waist:Hip Ratio', 'TSH (mIU/L)', 'AMH(ng/mL)', 'PRL(ng/mL)', 'Vit D3 (ng/mL)', 'PRG(ng/mL)', 'RBS(mg/dl)', 'Weight gain(Y/N)', 'hair growth(Y/N)', 'Skin darkening (Y/N)', 'Hair loss(Y/N)', 'Pimples(Y/N)', 'Fast food (Y/N)', 'Reg.Exercise(Y/N)', 'BP _Systolic (mmHg)', 'BP _Diastolic (mmHg)', 'Follicle No. (L)', 'Follicle No. (R)', 'Avg. F size (L) (mm)', 'Avg. F size (R) (mm)', 'Endometrium (mm)']
+
+values = [33, 89.0, 163.0, 33.5, 12, 78, 22, 10.8, 4, 7, 6.0, 1, 0, 381.99, 15.0, 3.45, 0.72, 4.791666667, 46, 40, 0.8695652174, 0.73, 0.7, 15.23, 16.7, 0.28, 100.0, 1, 0, 1, 1, 1, 1.0, 0, 120, 80, 13, 12, 19.0, 18.0, 7.9]
+
+# ========================================================================================
+
+# Ovarian Cancer Setup
+
+target_names = {
+    'CC': "Clear Cell Carcinoma",
+    'EC': "Endometrioid Carcinoma",
+    'HGSC': "High Grade Serous Carcinoma",
+    'LGSC': "Low Grade Serous Carcinoma",
+    'MC': 'Mucinous Carcinoma'
+}
+
+model = tf.keras.models.load_model("models/ovarian-cancer.h5")
+
+def load_and_prep_image(filename, img_shape=224):
+    # Read in target file (an image)
+    img = tf.io.read_file(filename)
+    # Decode the read file into a tensor & ensure 3 colour channels
+    # (our model is trained on images with 3 colour channels and sometimes images have 4 colour channels)
+    img = tf.image.decode_image(img, channels=3)
+    # Resize the image (to the same size our model was trained on)
+    img = tf.image.resize(img, size = [img_shape, img_shape])
+    # Rescale the image (get all values between 0 and 1)
+    img = img/255.
+    return img
+
+def pred_and_plot(model, filename, target_names):
+    tn = list(target_names.keys())
+    """
+    Imports an image located at filename, makes a prediction on it with
+    a trained model and plots the image with the predicted class as the title.
+    """
+    # Import the target image and preprocess it
+    img = load_and_prep_image(filename)
+    # Make a prediction
+    pred = model.predict(tf.expand_dims(img, axis=0))
+    # Get the predicted class
+    pred_class = tn[int(tf.round(pred)[0][0])]
+
+    return f"The class of Ovarian Cancer is {target_names[pred_class]}"
+
+# ========================================================================================
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/cervical-cancer", methods=["GET", "POST"])
+def cervical_cancer():
+    values = []
+    if request.method == "POST":
+        for item in mainsfeatures:
+            resp = request.form.get(item)
+            if resp != None:
+                values.append(resp)
+        sum = 0
+        for item in features[1]:
+            sum += (int(request.form.get(item)))
+        if sum>0:
+            values.insert(8, "1")
+            values.insert(9, str(sum))
+        else:
+            values.insert(8, "0")
+            values.insert(9, str(sum))
+        vals = [float(value) for value in values]
+        prediction = model.predict(np.array([vals]))
+        
+        return render_template("cervical-cancer.html", features=features, values=[], cc_result=str(prediction[0]*25) + "%")
+
+    return render_template("cervical-cancer.html", features=features, values=values, cc_result="")
+
+@app.route("/pcos", methods=["GET", "POST"])
+def pcos():
+    if request.method == "POST":
+        pred = [[]]
+        for item in pcos_features:
+            pred[0].append(request.form.get(item))
+
+        pcos_model = joblib.load(r"models\pcos.joblib")
+        prediction = list(pcos_model.predict(pred))[0]
+        if prediction == 1:
+            return render_template("pcos.html", pcos_features=pcos_features, values=values, pcos_result="There is a high chance of PCOS for these parameters.")
+        else:
+            return render_template("pcos.html", pcos_features=pcos_features, values=values, pcos_result="There is a low chance of PCOS for these parameters.")
+    
+    return render_template("pcos.html", pcos_features=pcos_features, values=values, pcos_result="")
+
+@app.route("/breast-cancer", methods=["GET", "POST"])
+def breast_cancer():
+    if request.method == "POST":
+        image = request.files["img"]
+        img_file_path = save_and_get_pred_img(image)
+        predict_img = Api_service(img_file_path)
+        has_cancer, has_no_cancer = predict_img.prediction_function()
+        return render_template("breast-cancer.html", bc_result=has_cancer)
+    return render_template("breast-cancer.html", bc_result="")
+    
+@app.route("/ovarian-cancer", methods=["GET", "POST"])
+def ovarian_cancer():
+    if request.method == "POST":
+        image = request.files["img"]
+        filename = secure_filename(image.filename)
+        file_path = os.path.join('Uploads', filename)
+        image.save(file_path)
+        result = pred_and_plot(model, file_path, target_names)
+        os.remove(file_path)
+        return render_template("ovarian-cancer.html", oc_result=result)
+    return render_template("ovarian-cancer.html", oc_result="")
+    
+@app.route("/journal", methods=["GET", "POST"])
+def journal():
+    # From MongoDB
+    articles = mongo.db.articles.find({})
+    # From JSON
+    # articles = data
+    return render_template("journal.html", articles=articles)
+
+@app.route("/article/<article_slug>")
+def article(article_slug):
+    # From MongoDB
+    article = mongo.db.articles.find_one({ "slug": article_slug })
+    # From JSON
+    # article = None
+    # for item in data:
+    #     if item["slug"] == article_slug:
+    #         article = item
+    return render_template("article.html", article=article)
+
+@app.route("/add", methods=["POST"])
+def add():
+    article = request.json
+    mongo.db.articles.insert_one(article)
+    return "Article added successfully."
+
+@app.route("/get", methods=["GET", "POST"])
+def get():
+    reqUrl = "https://femcare-chatbot.vercel.app/get"
+    msg = request.form["msg"]
+
+    headersList = {
+    "Accept": "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    "Content-Type": "multipart/form-data; boundary=kljmyvW1ndjXaOEAg4vPm6RBUqO6MC5A" 
+    }
+
+    payload = f"--kljmyvW1ndjXaOEAg4vPm6RBUqO6MC5A\r\nContent-Disposition: form-data; name=\"msg\"\r\n\r\n{msg}\r\n--kljmyvW1ndjXaOEAg4vPm6RBUqO6MC5A--\r\n"
+
+    response = requests.request("POST", reqUrl, data=payload,  headers=headersList)
+    return response.text
+
+if __name__ == "__main__":
+    app.run(debug=True, host="192.168.0.104", port=5001)
